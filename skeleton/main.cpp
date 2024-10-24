1183a804384aab4235e68ba44668710a42706fe6
Nahia <82345386+nahigles@users.noreply.github.com>
2024-10-25 01:27:58 +0200

    Ultimo retoque

--- skeleton/main.cpp
+++ skeleton/main.cpp
@@ -216,13 +216,13 @@ void keyPress(unsigned char key, const PxTransform& camera)
 	}
 	case 'T': {
 		// LLUVIA
-		particleSystems.push_back(new ParticleSystem({ 0.0,30.0,0.0 }, { 0.0, -10.0, 0.0 }, { 0.0, -15.0, 0.0 }, 0.25, { 0.0, 0.56, 0.9, 0.3 }, 9.0, 0.05, 20, 5, RAIN));
+		particleSystems.push_back(new ParticleSystem({ 0.0,50.0,0.0 }, { 0.0, -10.0, 0.0 }, { 0.0, -20.0, 0.0 }, 0.25, { 0.0, 0.56, 0.9, 0.3 }, 9.0, 0.05, 20, 5, RAIN));
 
 		break;
 	}
 	case 'E': {
 		// NIEVE
-		particleSystems.push_back(new ParticleSystem({ 0.0,50.0,0.0 }, { 0.0, -10.0, 0.0 }, { 0.0, -1.0, 0.0 }, 0.5, { 1.0, 1.0, 1.0, 0.0 }, 9.0, 0.05, 20, 5, SNOW));
+		particleSystems.push_back(new ParticleSystem({ 0.0,50.0,0.0 }, { 0.0, -10.0, 0.0 }, { 0.0, -0.5, 0.0 }, 0.5, { 1.0, 1.0, 1.0, 0.0 }, 9.0, 0.05, 20, 5, SNOW));
 		break;
 	}
 	case 'R':
